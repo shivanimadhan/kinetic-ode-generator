f0005f061a932a93c05b0ada7866717e6628bad2
Devon <devoncallan@ucsb.edu>
2023-11-06 14:12:59 -0800

    Refactored reactions classes

--- reactions.py
+++ reactions.py
@@ -1,6 +1,7 @@
 
 from dataclasses import dataclass
 import math
+from typing import List, Dict
 
 from species import Species
 
@@ -36,96 +37,91 @@ class ReactionSpecies:
     species:Species
     coeff:int
 
-
-# class Reaction:
-
-#     def __init__(self, rate_constant: RateConstant,
-#             num_unit_reactants:int, num_poly_reactants:int,
-#             num_unit_products:int,  num_poly_products:int):
-#         self.rate_constant = rate_constant
-
-#         self.unit_reactant_species = [] * num_unit_reactants
-#         self.poly_reactant_species = [] * num_poly_reactants
-        
-#         self.unit_product_species = [] * num_unit_products
-#         self.poly_product_species = [] * num_poly_products
-
-#     def clean(self):
-
-#         self.reactant_species = self.unit_reactant_species + self.poly_reactant_species
-#         self.product_species = self.unit_product_species + self.poly_product_species
-#         self.all_species = self.reactant_species + self.product_species
-
-#     def calculate_rate(self, c):
-
-#         # Assumes elementary reactions
-#         rate = self.rate_constant
-#         for reactant_species in self.reactant_species:
-#             rate *= reactant_species.coeff * (c[reactant_species.name] ** reactant_species.coeff)
-#         return rate
-    
-#     # def get_rate_string(self):
-#     #     rate_strings = [self.rate_constant]
-#     #     for reactant_species in self.reactant_species:
-#     #         rate_strings.append(f'{reactant_species.coeff}*{c[reactant_species.name]}**{reactant_species.coeff}')
-#     #     rate_string = rate_strings.join('*')
-#     #     return rate_string
-            
-# class InitiatorDecompositionReaction(Reaction):
-
-#     def __init__(self, rate_constant: RateConstant, 
-#         unit_reactant, unit_product):
-#         super().__init__(rate_constant, 1, 0, 2, 0)
-
-#         self.unit_reactants[0] = unit_reactant
-#         self.unit_products[0]  = unit_product
-#         self.unit_products[1]  = unit_product
-
-#     def calculate_rate(self, c:dict):
-
-#         return self.rate_constant.get_k() * c[self.unit_reactants[0]]
-    
-# class PropagationReaction(Reaction):
-
-#     def __init__(self, rate_constant: RateConstant,
-#         unit_reactant, poly_reactant, poly_product):
-#         super().__init__(rate_constant, 1, 1, 0, 1)
-
-#         self.unit_reactants[0] = unit_reactant
-#         self.poly_reactants[0] = poly_reactant
-#         self.poly_products[0]  = poly_product
-
-#     def calculate_rate(self, c:dict):
-
-#         return self.rate_constant.get_k() * c[self.unit_reactants[0]] * c[self.poly_reactants[0]]
-
-# class TerminationCombinationReaction(Reaction):
-
-#     def __init__(self, rate_constant: RateConstant,
-#         poly_reactant1, poly_reactant2, poly_product):
-#         super().__init__(rate_constant, 0, 2, 0, 1)
-
-#         self.poly_reactants[0] = poly_reactant1
-#         self.poly_reactants[1] = poly_reactant2
-
-#         self.poly_products[0] = poly_product
-
-#     def calculate_rate(self, c:dict):
-
-#         return self.rate_constant.get_k() * c[self.poly_reactants[0]] * c[self.poly_reactants[1]]
-
-# class TerminationDisproportionationReaction(Reaction):
+class Reaction:
 
     def __init__(self, rate_constant: RateConstant,
-        poly_reactant1, poly_reactant2, poly_product1, poly_product2):
-        super().__init__(rate_constant, 0, 2, 0, 2)
+                 unit_reactant_species:List[ReactionSpecies], poly_reactant_species:List[ReactionSpecies],
+                 unit_product_species:List[ReactionSpecies], poly_product_species:List[ReactionSpecies]):
+        self.rate_constant = rate_constant
 
-        self.poly_reactants[0] = poly_reactant1
-        self.poly_reactants[1] = poly_reactant2 
+        self.unit_reactant_species = unit_reactant_species
+        self.poly_reactant_species = poly_reactant_species
+        self.unit_product_species = unit_product_species
+        self.poly_product_species = poly_product_species
 
-        self.poly_products[0] = poly_product1
-        self.poly_products[1] = poly_product2
+        self.reactant_species = self.unit_reactant_species + self.poly_reactant_species
+        self.product_species = self.unit_product_species + self.poly_product_species
+        self.all_species = self.reactant_species + self.product_species
 
-    def calculate_rate(self, c:dict):
+    def calculate_rate(self, c: Dict[str, float]) -> float:
 
-        return self.rate_constant.get_k() * c[self.poly_reactants[0]] * c[self.poly_products[1]]      
+        # Assumes elementary reactions
+        rate = self.rate_constant.get_k()
+        for reactant_species in self.reactant_species:
+            rate *= reactant_species.coeff * (c[reactant_species.name] ** reactant_species.coeff)
+        return rate
+    
+class InitiatorDecompositionReaction(Reaction):
+
+    def __init__(self, rate_constant: RateConstant, 
+                 unit_reactant: ReactionSpecies, 
+                 unit_product: ReactionSpecies):
+        super().__init__(
+            rate_constant=rate_constant, 
+            unit_reactant_species=[unit_reactant], 
+            poly_reactant_species=[],
+            unit_product_species=[unit_product],
+            poly_product_species=[]
+        )
+
+class InitiationReaction(Reaction):
+
+    def __init__(self, rate_constant: RateConstant,
+                 unit_reactant1: ReactionSpecies, unit_reactant2: ReactionSpecies, 
+                 poly_product: ReactionSpecies):
+        super().__init__(
+            rate_constant=rate_constant,
+            unit_reactant_species=[unit_reactant1, unit_reactant2],
+            poly_reactant_species=[],
+            unit_product_species=[],
+            poly_product_species=[poly_product]
+        )
+
+class PropagationReaction(Reaction):
+
+    def __init__(self, rate_constant: RateConstant,
+                 unit_reactant: ReactionSpecies, poly_reactant: ReactionSpecies, 
+                 poly_product: ReactionSpecies):
+        super().__init__(
+            rate_constant=rate_constant,
+            unit_reactant_species=[unit_reactant],
+            poly_reactant_species=[poly_reactant],
+            unit_product_species=[],
+            poly_product_species=[poly_product]
+        )
+
+class TerminationCombinationReaction(Reaction):
+
+    def __init__(self, rate_constant: RateConstant,
+                 poly_reactant1: ReactionSpecies, poly_reactant2: ReactionSpecies, 
+                 poly_product: ReactionSpecies):
+        super().__init__(
+            rate_constant=rate_constant,
+            unit_reactant_species=[],
+            poly_reactant_species=[poly_reactant1, poly_reactant2],
+            unit_product_species=[],
+            poly_product_species=[poly_product]
+        )
+
+class TerminationDisproportionationReaction(Reaction):
+
+    def __init__(self, rate_constant: RateConstant,
+                 poly_reactant1: ReactionSpecies, poly_reactant2: ReactionSpecies, 
+                 poly_product: ReactionSpecies):
+        super().__init__(
+            rate_constant=rate_constant,
+            unit_reactant_species=[],
+            poly_reactant_species=[poly_reactant1, poly_reactant2],
+            unit_product_species=[],
+            poly_product_species=[poly_product]
+        )
