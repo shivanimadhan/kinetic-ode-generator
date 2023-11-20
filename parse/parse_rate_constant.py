97d5605541389582acb34685b64afa57303a3cbb
Shivani Madhan <madhan.shivani1104@gmail.com>
2023-11-20 12:32:56 -0800

    updated parse_rate_constant.py

--- parse/parse_rate_constant.py
+++ parse/parse_rate_constant.py
@@ -2,32 +2,40 @@ import re
 from parse.parse_input import parse_args_kwargs
 from reactions import RateConstant
 
-def parse_all_constants(constants_section:list):
-    list_of_all_constants = []
+class RateConstantParser:
 
-    for constants_line in constants_section:
-        constant = parse_constants_string(constants_line)
-        list_of_all_constants.append(constant)
+    all_constants = [];
 
-    return list_of_all_constants
+    def __init__(self):
+        pass
 
-def parse_constants_string(constants_str:str):
-    args, kwargs = parse_args_kwargs(constants_str)
-    
-    name_str = args
-    if 'k' in kwargs:
-        kwargs['k'] = float(kwargs['k'])
-    if 'f' in kwargs:
-        kwargs['f'] = float(kwargs['f'])
-    if 'A' in kwargs:
-        kwargs['A'] = float(kwargs['A'])
-    if 'E' in kwargs:
-        kwargs['E'] = float(kwargs['E'])
+    def parse_all_constants(constants_section:list):
+        for constants_line in constants_section:
+            constant = RateConstantParser.parse_constants_string(constants_line)
+            RateConstantParser.all_constants.append(constant)
 
-    constant = RateConstant(name_str, **kwargs)
-    return constant
+    def parse_constants_string(constants_str:str):
+        args, kwargs = parse_args_kwargs(constants_str)
+        
+        name_str = args
+        if 'k' in kwargs:
+            kwargs['k'] = float(kwargs['k'])
+        if 'f' in kwargs:
+            kwargs['f'] = float(kwargs['f'])
+        if 'A' in kwargs:
+            kwargs['A'] = float(kwargs['A'])
+        if 'E' in kwargs:
+            kwargs['E'] = float(kwargs['E'])
 
-def parse_reaction_constant(reaction_string):
-    constant = RateConstant()
-    return constant
-    
+        constant = RateConstant(name_str, **kwargs)
+        return constant
+
+    def parse_reaction_constant(reaction_string):
+        name = re.compile(r'-(\w+)->')
+        match = name.search(reaction_string)
+
+        for c in RateConstantParser.all_constants:
+            if (match.group(1) == c.name[0]):
+                return c
+
+        
--- parse/parse_reactions.py
+++ parse/parse_reactions.py
@@ -1,68 +1,57 @@
 import re
 from reactions import ReactionSpecies
-from parse.parse_rate_constant import parse_reaction_constant
+from parse.parse_rate_constant import RateConstantParser
 from typing import List
 
-# Address this function
-def parse_all_reactions(reactions_section:List[str], all_species):
-    return
-    # call parse_reaction_string similar to in species
-    # list_of_all_reactions = []
+class ReactionParser:
+    def __init__(self, species_list):
+        self.all_species = species_list
+    # Address this function
+    def parse_all_reactions(reactions_section:List[str]):
+        return
 
-    # for reactions_line in reactions_section:
+    def parse_reaction_string(reaction_string):
+        # Parse reaction type
 
-    #     reaction = parse_reaction_string(reactions_line, all_species)
-    #     list_of_all_reactions.append(reaction)
+        # Split the reaction string into reactant and product sides
+        reactant_side, product_side = re.split(r'\s*-\w*->\s*', reaction_string)
 
-    # return list_of_all_reactions
+        # Parse the reactant side
+        # What happens if there's only one reactant? 
+        reactants = re.split(r'\s*\+\s*', reactant_side)
+        reactant_species = [ReactionParser.parse_reaction_species_string(reactant) for reactant in reactants]
 
-def parse_reaction_string(reaction_string, all_species):
-    # Parse reaction type
+        # Parse the product side
+        products = re.split(r'\s*\+\s*', product_side)
+        product_species = [ReactionParser.parse_reaction_species_string(product) for product in products]
 
-    # Split the reaction string into reactant and product sides
-    reactant_side, product_side = re.split(r'\s*-\w*->\s*', reaction_string)
+        #Call parse_rate_constant
+        rate_constant = RateConstantParser.parse_reaction_constant(reaction_string)
 
-    # Parse the reactant side
-    # What happens if there's only one reactant? 
-    reactants = re.split(r'\s*\+\s*', reactant_side)
-    reactant_species = [parse_reaction_species_string(reactant, all_species) for reactant in reactants]
+        return reactant_species, product_species, rate_constant
+    # Address this function
+    def parse_reaction_species_string(reaction_species_string):
+        # Define a regular expression pattern to match the number and the rest of the string
+        pattern = r'^(\d*)(.*)$'
 
-    # Parse the product side
-    products = re.split(r'\s*\+\s*', product_side)
-    product_species = [parse_reaction_species_string(product, all_species) for product in products]
+        # Use re.match() to find the match at the beginning of the string
+        match = re.match(pattern, reaction_species_string)
 
-    #Call parse_rate_constant
-    rate_constant = parse_reaction_constant(reaction_string)
+        if match:
+            coeff_part = match.group(1)
+            name = match.group(2)
+            if coeff_part:
+                coeff = int(coeff_part)
+            else:
+                coeff = 1
 
-    return reactant_species, product_species, rate_constant
-# Address this function
-def parse_reaction_species_string(reaction_species_string, all_species):
-    # Define a regular expression pattern to match the number and the rest of the string
-    pattern = r'^(\d*)(.*)$'
+        # Match the name with the species
+        for s in all_species:
+            if (name == s.name):
+                species = ReactionSpecies(s, coeff)
+                return species
 
-    # Use re.match() to find the match at the beginning of the string
-    match = re.match(pattern, reaction_species_string)
+        species = ReactionSpecies(None, coeff)
+        return species
 
-    if match:
-        coeff_part = match.group(1)
-        name = match.group(2)
-        if coeff_part:
-            coeff = int(coeff_part)
-        else:
-            coeff = 1
-
-    #     print("Coefficient:", coeff)
-    #     print("Name:", name)
-    # else:
-    #     print("No match found.")
-    
-    # Match the name with the species
-    for s in all_species:
-        if (name == s.name):
-            species = ReactionSpecies(s, coeff)
-            return species
-
-    species = ReactionSpecies(None, coeff)
-    return species
-
-# Create additional reactions for equivalent species
+    # Create additional reactions for equivalent species
